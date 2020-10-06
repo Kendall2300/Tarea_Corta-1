05fde6f558361925e5c1b559034799fa9709056d
DanielMontoyaR <62631584+DanielMontoyaR@users.noreply.github.com>
2020-10-05 19:20:41 -0600

    Se agrega documentación de la clase ShareCar.

--- Codigo/src/ShareCar/CheckFacade.java
+++ Codigo/src/ShareCar/CheckFacade.java
@@ -1,13 +1,23 @@
 package ShareCar;
 
+/**
+*The class calls the classes SearchCar and Disponibilidad
+*/
 public class CheckFacade {
     private SearchCar carro;
     private Disponibilidad Disposicion;
-
+    
+    /**
+    *The method creates the instance of the two classes
+    */
     public CheckFacade(){
         carro=new SearchCar();
         Disposicion=new Disponibilidad();
     }
+    
+    /**
+    *The method recieves the departure and return date data as parameter and calls out the other two clases.
+    */
     public void search(String fechaIda, String fechaVuelta){
         carro.searchCar(fechaIda, fechaVuelta);
         Disposicion.searchDisponibilidad(fechaIda, fechaVuelta);
