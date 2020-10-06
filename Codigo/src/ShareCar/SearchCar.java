bbf54782719dcc444a129d5b5413da9cae079906
DanielMontoyaR <62631584+DanielMontoyaR@users.noreply.github.com>
2020-10-05 19:36:19 -0600

    Se agregó la documentación a la clase SearchCar.

--- Codigo/src/ShareCar/SearchCar.java
+++ Codigo/src/ShareCar/SearchCar.java
@@ -1,5 +1,8 @@
 package ShareCar;
 
+/**
+*The class represents the search for available vehicles according to certain dates.
+*/
 public class SearchCar {
     public void searchCar(String fechaIda, String fechaVuelta){
         System.out.println("======================");
